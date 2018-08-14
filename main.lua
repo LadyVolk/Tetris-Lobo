70185ab6f72cb903820568743c655e5d74dd82b4
LadyVolk <paa.lobo@protonmail.com>
2018-08-14 13:42:16 -0300

    Game box
    centralized game box
    changed color of background
    added  coments

--- main.lua
+++ main.lua
@@ -10,6 +10,8 @@ function love.update(dt)
 end
 
 function love.draw()
-  love.graphics.setColor(Colors.unpack(COLOR.vazio))
+  love.graphics.setColor(Colors.unpack(COLOR.background))
   love.graphics.rectangle("fill", 0, 0, WIN_W, WIN_H)
+  love.graphics.setColor(Colors.unpack(COLOR.vazio))
+  love.graphics.rectangle("fill", -(GRID_W*TILE_W-WIN_W)/2, -(GRID_H*TILE_H-WIN_H)/2, GRID_W*TILE_W, GRID_H*TILE_H)
 end
--- setup.lua
+++ setup.lua
@@ -1,15 +1,17 @@
 local Colors = require "colors"
 local function setup()
+  --number of tiles
   GRID_W = 10
   GRID_H = 20
+  --real size in pixels
   TILE_W = 10
   TILE_H = 10
   COLOR = {
-    vazio = Colors.rgb(195, 229, 203),
+    vazio = Colors.rgb(66, 86, 244),
     background = Colors.rgb(177, 219, 237)
   }
+  --get dimentsions of window
   WIN_W, WIN_H = love.graphics.getDimensions()
-  print("terminei de fazer setup")
 end
 
 return setup
