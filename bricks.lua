a539b7bef034231b00db941c08fbaa121f67e17d
LadyVolk <paa.lobo@protonmail.com>
2018-08-14 14:20:02 -0300

    pieces
    tableas para cada tipo de peça

---
+++ bricks.lua
@@ -0,0 +1,64 @@
+local Colors = require "colors"
+local square = {
+  coord = {
+    {0, 0},
+    {0, 1},
+    {1, 0},
+    {1, 1}
+  },
+  cor = Colors.rgb(206, 66, 244)
+}
+local line = {
+  coord = {
+    {0, 0},
+    {1, 0},
+    {2, 0},
+    {3, 0},
+  },
+  cor = Colors.rgb(14, 23, 25)
+}
+local s = {
+  coord = {
+    {0, 1},
+    {0, 2},
+    {1, 0},
+    {1, 1},
+  },
+  cor = Colors.rgb(2, 221, 20)
+}
+local z = {
+  coord = {
+    {0, 0},
+    {0, 1},
+    {1, 1},
+    {1, 2},
+  },
+  cor = Colors.rgb(224, 176, 4)
+}
+local l = {
+  coord = {
+    {0, 0},
+    {1, 0},
+    {2, 0},
+    {2, 1},
+  },
+  cor = Colors.rgb(224, 4, 4)
+}
+local j = {
+  coord = {
+    {0, 1},
+    {1, 1},
+    {2, 0},
+    {2, 1},
+  },
+  cor = Colors.rgb(244, 244, 244)
+}
+local t = {
+  coord = {
+    {0, 0},
+    {0, 1},
+    {0, 2},
+    {1, 1},
+  },
+  cor = Colors.rgb(110, 124, 193)
+}
